5d0113d658e9bda9ed9cfa818a4ee21fbc4952f9
Meme Man <yuyuss210@gmail.com>
2026-09-11 00:35:09 +0300

    some attacks
    igb

--- src/main/java/dev/ploats/slopstrocity/content/entity/boss/Slopstrocity.java
+++ src/main/java/dev/ploats/slopstrocity/content/entity/boss/Slopstrocity.java
@@ -1,7 +1,11 @@
 package dev.ploats.slopstrocity.content.entity.boss;
 
+import dev.ploats.slopstrocity.api.vfx.ScreenShakeEffect;
+import dev.ploats.slopstrocity.content.entity.ai.goal.hostile.AnimatableAttackGoal;
+import dev.ploats.slopstrocity.content.entity.ai.goal.hostile.BandaidMoveToTargetGoal;
 import dev.ploats.slopstrocity.content.entity.base.AnimatableBoss;
 import dev.ploats.slopstrocity.content.registry.SlopstrocitySoundEvents;
+import dev.ploats.slopstrocity.util.MathUtil;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -12,16 +16,21 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.fluids.FluidType;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -68,7 +77,71 @@ public class Slopstrocity extends AnimatableBoss {
 
     @Override
     protected void registerGoals() {
+        goalSelector.addGoal(0, new BandaidMoveToTargetGoal(this, 1.4D)
+                .satisfactoryDist(3.5D));
         goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.1D));
+
+        goalSelector.addGoal(0, new AnimatableAttackGoal<>(this, ObjectArrayList.of(SLOP_SLAM_ATTACK_ANIM), 48.8D, true, SLOP_SLAM_ATTACK_ID)
+                .attackArc(360.0D)
+                .potentialTargetRadius(12.0D)
+                .attackFrame(22.8D, 25.0D)
+                .attackTickCooldown(4.0D)
+                .initiationRange(7.0D)
+                .performDefaultAttack(false)
+                .additionalStartConditions((animatable) -> animatable.random.nextDouble() >= 0.9D)
+                .actionOnStart((animatable, target, potentialTargets, curTick) -> {
+                    animatable.stopAnimation(IDLE_ANIM);
+                })
+                .actionOnAttack((animatable, target, potentialTargets, curTick) -> {
+                    new ScreenShakeEffect(animatable.blockPosition(), 27.5D, 0.0108F, 49.5F, 1.1F).enqueue(animatable.level());
+
+                    if (curTick == 23.0D) playSound(SlopstrocitySoundEvents.SLOPSTROCITY_SLOP_SLAM_ATTACK.get());
+
+                    hurtTargets(animatable, target, potentialTargets);
+                })
+                .actionOnEnd((animatable, target, potentialTargets, curTick) -> animatable.playAnimation(IDLE_ANIM, true)));
+
+        goalSelector.addGoal(0, new AnimatableAttackGoal<>(this, ObjectArrayList.of(SLOP_STOMP_LEFT_ATTACK_ANIM, SLOP_STOMP_RIGHT_ATTACK_ANIM), 58.4D, true, SLOP_STOMP_ATTACK_ID)
+                .attackArc(360.0D)
+                .potentialTargetRadius(12.0D)
+                .attackFrame(30.8D, 33.4D)
+                .attackTickCooldown(4.0D)
+                .initiationRange(9.0D)
+                .performDefaultAttack(false)
+                .additionalStartConditions((animatable) -> animatable.random.nextDouble() >= 0.84D)
+                .actionOnStart((animatable, target, potentialTargets, curTick) -> {
+                    animatable.stopAnimation(IDLE_ANIM);
+                })
+                .actionOnAttack((animatable, target, potentialTargets, curTick) -> {
+                    new ScreenShakeEffect(animatable.blockPosition(), 23.5D, 0.0088F, 45.5F, 1.0F).enqueue(animatable.level());
+
+                    if (curTick == 31.0D) playSound(SlopstrocitySoundEvents.SLOPSTROCITY_SLOP_STOMP_ATTACK.get());
+
+                    hurtTargets(animatable, target, potentialTargets);
+                })
+                .actionOnEnd((animatable, target, potentialTargets, curTick) -> animatable.playAnimation(IDLE_ANIM, true)));
+
+        targetSelector.addGoal(0, new HurtByTargetGoal(this));
+        targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, Player.class, true));
+    }
+
+    private void hurtTargets(Slopstrocity animatable, @Nullable LivingEntity target, List<LivingEntity> potentialTargets) {
+        if (target != null && !target.noPhysics) hurtTargetAngularly(animatable, target);
+
+        for (LivingEntity potentialTarget : potentialTargets) {
+            if (potentialTarget == null || potentialTarget.noPhysics || potentialTarget == this || potentialTarget == target) continue;
+
+            hurtTargetAngularly(animatable, potentialTarget);
+        }
+    }
+
+    private void hurtTargetAngularly(Slopstrocity animatable, @Nullable LivingEntity target) {
+        target.hurt(level().damageSources().mobAttack(animatable), Math.min(5.0F, 37.5F - animatable.distanceTo(target)));
+
+        double targetAngle = (MathUtil.getAngleBetweenEntities(animatable, target) + 90) * Math.PI / 180;
+        double kbMultiplier = -2.22D;
+
+        target.setDeltaMovement(kbMultiplier * Math.cos(targetAngle), target.getDeltaMovement().normalize().y + (random.nextDouble() * 2 + 0.2D), kbMultiplier * Math.sin(targetAngle));
     }
 
     @Override
@@ -154,6 +227,11 @@ public class Slopstrocity extends AnimatableBoss {
     @Override
     protected float getJumpPower() {
         return 0.0F;
+    }
+
+    @Override
+    public boolean isAttackingStatically() {
+        return super.isAttackingStatically() && getAttackId() != ROLLING_BLUNDER_ATTACK_ID;
     }
 
     @Override
